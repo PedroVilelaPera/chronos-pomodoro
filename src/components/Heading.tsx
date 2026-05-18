2921ced5cc0912a79b15cd44bc40318ca55427e4
Pedro Pera <pedro.vpera@gmail.com>
2026-05-18 13:10:00 -0300

    fix: altera tipagem das props do Heading para receber o botao

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,10 @@
 import styles from './Heading.module.css';
 
-export function Heading(props) {
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps) {
   console.log('Heading component rendered with props:', props);
   return <h1 className={styles.heading}>{props.children}</h1>;
 }
