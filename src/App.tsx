d9aec3804950403ef80edbdab8b865cab4c72b6d
Pedro Pera <pedro.vpera@gmail.com>
2026-05-18 13:09:18 -0300

    feat: adiciona botão ao Heading teste

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { Heading } from './components/Heading';
 // CSS imports
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
       <Heading>
         Hello World! How is it going?
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>
         This is a simple React application that serves as a starting point for
