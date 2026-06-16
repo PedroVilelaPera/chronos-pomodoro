f8160d89896c4d21ba8353cac71cea43efd929d7
Pedro Pera <pedro.vpera@gmail.com>
2026-06-16 12:00:51 -0300

    feat: adicionar componente container

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@
 import './styles/theme.css';
 import './styles/global.css';
 
+// Components
+import { Container } from './components/Container';
 export function App() {
   return (
     <>
+      <Container>
+      </Container>
     </>
   );
 }
