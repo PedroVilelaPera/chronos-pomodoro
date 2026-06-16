e092ec5d19531816d29a6caa15c8943faeb13282
Pedro Pera <pedro.vpera@gmail.com>
2026-06-16 11:59:09 -0300

    refactor: remover importações e código de exemplo de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-// Components
-import { Heading } from './components/Heading';
-
 // CSS imports
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hello World! How is it going?
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        This is a simple React application that serves as a starting point for
-        building a more complex app. You can modify this component to add your
-        own functionality and styles.
-      </p>
     </>
   );
 }
