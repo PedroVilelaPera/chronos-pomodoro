4df8eb8738874d8dc5c6ef27d0d0dc2ccd258026
Pedro Pera <pedro.vpera@gmail.com>
2026-06-16 12:05:13 -0300

    feat: adicionar componente countdown

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 export function App() {
   return (
     <>
       <Container>
         <Logo />
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
       </Container>
     </>
