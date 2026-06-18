5dfb2a200e2949693ed3479afbde5a943830e318
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-06-18 14:15:58 -0300

    feat: implementa página Home

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,8 @@ import './styles/theme.css';
 import './styles/global.css';
 
 // Components
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
